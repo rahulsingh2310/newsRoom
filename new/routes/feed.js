da203a42e71349b13cd697a27a5610996dac87d1
ravishk17 <44134682+ravishk17@users.noreply.github.com>
2019-12-11 19:22:23 +0530

    recommendations

--- new/routes/feed.js
+++ new/routes/feed.js
@@ -1,70 +1,71 @@
-const express = require("express");
-const { body } = require("express-validator");
+const express = require('express');
+const { body } = require('express-validator');
 
-const feedController = require("../controllers/feed");
-const isAuth = require("../middleware/is-auth");
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 //GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get('/posts', feedController.getPosts);
 
-router.get("/posts/:tag", feedController.getbyTag);
+router.get('/posts/:tag', feedController.getbyTag);
 
 //POST /feed/post
 router.post(
-  "/post",
-  isAuth,
-  [
-    body("title")
-      .trim()
-      .isLength({ min: 5 }),
-    body("content")
-      .trim()
-      .isLength({ min: 5 })
-  ],
-  feedController.createPost
+	'/post',
+	isAuth,
+	[
+		body('title')
+			.trim()
+			.isLength({ min: 5 }),
+		body('content')
+			.trim()
+			.isLength({ min: 5 })
+	],
+	feedController.createPost
 );
 
-router.get("/post/:postId", isAuth, feedController.getPost);
+router.get('/post/:postId', isAuth, feedController.getPost);
 
 router.put(
-  "/post/:postId",
-  isAuth,
-  [
-    body("title")
-      .trim()
-      .isLength({ min: 5 }),
-    body("content")
-      .trim()
-      .isLength({ min: 5 })
-  ],
-  feedController.updatePost
+	'/post/:postId',
+	isAuth,
+	[
+		body('title')
+			.trim()
+			.isLength({ min: 5 }),
+		body('content')
+			.trim()
+			.isLength({ min: 5 })
+	],
+	feedController.updatePost
 );
 
-router.delete("/post/:postId", isAuth, feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 router.delete(
-  "/post/deletecomment/:commentId",
-  isAuth,
-  feedController.deleteComment
+	'/post/deletecomment/:commentId',
+	isAuth,
+	feedController.deleteComment
 );
 
 router.post(
-  "/post/comment/:postId",
-  isAuth,
-  [
-    body("reaction")
-      .trim()
-      .isLength({ min: 1 })
-  ],
-  feedController.makeComment
+	'/post/comment/:postId',
+	isAuth,
+	[
+		body('reaction')
+			.trim()
+			.isLength({ min: 1 })
+	],
+	feedController.makeComment
 );
 
-router.post("/post/like/:postId", isAuth, feedController.likeHandler);
-router.post("/post/dislike/:postId", isAuth, feedController.dislikeHandler);
+router.post('/post/like/:postId', isAuth, feedController.likeHandler);
+router.post('/post/dislike/:postId', isAuth, feedController.dislikeHandler);
 
-router.get("/postss/grammarcheck", feedController.supplyPosts);
+router.get('/postss/grammarcheck', feedController.supplyPosts);
 
-router.post("/correctPosts", feedController.correctPosts);
+router.post('/correctPosts', feedController.correctPosts);
+router.get('/user_posts/:id', feedController.getbyUser);
 
 module.exports = router;
